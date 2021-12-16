c58513405a752c6b5523bcd6612dfb90bb84b619
JCGuest <jcguest90@gmail.com>
2021-12-15 23:04:28 -0600

    add aggregator mock from chainlink

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,11 +1,27 @@
-from brownie import FundMe
+from brownie import FundMe, MockV3Aggregator, config, network
 from scripts.helper_scripts import get_account
 
 
 def deploy_fund_me():
     account = get_account()
-    fund_me = FundMe.deploy({"from": account}, publish_source=True)
-    print(f"Deployed {fund_me.address}")
+    if network.show_active() != "development":
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else:
+        print(f"Active network: {network.show_active()}")
+        print("Deploying Mocks...")
+        mock_aggregator = MockV3Aggregator.deploy(
+            18, 2000000000000000000, {"from": account}
+        )
+        price_feed_address = mock_aggregator.address
+        print(f"Deployed! Price feed address: {price_feed_address}")
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
+    # print(f"Deployed {fund_me.address}")
 
 
 def main():
